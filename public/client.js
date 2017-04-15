47f6b542a499b264d9a3e8f62f4db858776fa2d9
Nick Ng <nick.doth@gmail.com>
2017-04-15 19:18:48 +0800

    encapsulate two page controllers

--- public/client.js
+++ public/client.js
@@ -2,33 +2,18 @@
 
 let app = $('#app');
 
-app.on('keyup', '.OrderPage .getById', (e) => {
-    if (e.keyCode === 13) {
-        location.href = '/order/view/' + e.target.value + '?filter=1';
-    }
-});
+// ====== OrderPageController ======
 
-app.on('submit', '.OrderPage form.handleOrder', (e) => {
-    e.preventDefault();
-    let form = e.target;
-    let action = form.dataset.action;
-    let payload = {};
-    Array.from($(form).find('input[action-payload]')).forEach(el => {
-        payload[el.name] = el.value;
-    });
+buildGetById(app, 'OrderPage', 'order');
+buildActionForm(app, 'OrderPage', 'order');
 
-    postJson('/order/handle/' + form.dataset.orderid, { type: action, payload })
-    .then(result => {
-        console.log(result);
-        // store.dispatch({ action: 'HANDLE_COMPLETE' });
-        location.reload();
-    })
-    .catch(err => {
-        console.error(err);
-        // store.dispatch({ action: 'HANDLE_FAIL' });
-    })
-});
+// ====== ProductPageController ======
 
+buildGetById(app, 'ProductPage', 'product');
+buildActionForm(app, 'ProductPage', 'product');
+
+
+// ====== Helpers ======
 
 function postJson(url, data) {
     return Promise.resolve($.post({
@@ -36,4 +21,37 @@ function postJson(url, data) {
         data: JSON.stringify(data),
         contentType: 'application/json'
     }));
+}
+
+// ====== Control Builders ======
+
+function buildGetById(app, controller, baseRoute) {
+    app.on('keyup', `.${controller} .getById`, (e) => {
+        if (e.keyCode === 13) {
+            location.href = `/${baseRoute}/view/` + e.target.value + '?filter=1';
+        }
+    });
+}
+
+function buildActionForm(app, controller, baseRoute) {
+    app.on('submit', `.${controller} form.handleOrder`, (e) => {
+        e.preventDefault();
+        let form = e.target;
+        let action = form.dataset.action;
+        let payload = {};
+        Array.from($(form).find('input[action-payload]')).forEach(el => {
+            payload[el.name] = el.value;
+        });
+
+        postJson(`/${baseRoute}/handle/` + form.dataset.id, { type: action, payload })
+        .then(result => {
+            console.log(result);
+            // store.dispatch({ action: 'HANDLE_COMPLETE' });
+            location.reload();
+        })
+        .catch(err => {
+            console.error(err);
+            // store.dispatch({ action: 'HANDLE_FAIL' });
+        })
+    });
 }
