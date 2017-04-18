236f698e003a61a3b65fa459ef85cecad09ab33d
Nick Ng <nick.doth@gmail.com>
2017-04-18 14:10:12 +0800

    routes/personnel

--- routes/personnel.js
+++ routes/personnel.js
@@ -1,3 +1,15 @@
-module.exports = (req, res) => {
-    res.send('Not implemented');
-}
+var express = require('express');
+var router = express.Router();
+
+var users = require('../storage/__fake').users;
+
+router.get('/', (req, res) => {
+    res.render({
+        title: 'HR',
+        personnelPage: {
+            users: users
+        }
+    });
+});
+
+module.exports = router;
